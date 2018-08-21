8a83f4475b41cc82c3a9e78681b026557ac8e7ff
Marcela Ceneviva Auslenter <cotchoninha@yahoo.com.br>
2018-08-21 15:08:13 +0100

    implemented SearchVC UI

---
+++ MarvelOus/CharacterSearchedCell.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterSearchedCell.swift
+//  MarvelOus
+//
+//  Created by Marcela Ceneviva Auslenter on 21/08/2018.
+//  Copyright © 2018 Marcela Ceneviva Auslenter. All rights reserved.
+//
+
+import UIKit
+
+class CharacterSearchedCell: UICollectionViewCell{
+    
+}
---
+++ MarvelOus/SearchCharacterView.swift
@@ -0,0 +1,28 @@
+//
+//  SearchCharacterView.swift
+//  MarvelOus
+//
+//  Created by Marcela Ceneviva Auslenter on 21/08/2018.
+//  Copyright © 2018 Marcela Ceneviva Auslenter. All rights reserved.
+//
+
+import UIKit
+
+
+class SearchCharacterView: UIViewController {
+    
+}
+
+extension SearchCharacterView: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "searchCell", for: indexPath) as! CharacterSearchedCell
+        return cell
+    }
+    
+    
+}
