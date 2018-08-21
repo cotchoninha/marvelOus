f20694eabc3c05dfe63a1bef555c7b0dca16518c
Marcela Ceneviva Auslenter <cotchoninha@yahoo.com.br>
2018-08-21 11:41:54 +0100

    implemented Table UI and created CV and TB files on XC

---
+++ MarvelOus/FavoritesTableView.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesTableView.swift
+//  MarvelOus
+//
+//  Created by Marcela Ceneviva Auslenter on 21/08/2018.
+//  Copyright © 2018 Marcela Ceneviva Auslenter. All rights reserved.
+//
+
+import Foundation
---
+++ MarvelOus/FavoritesTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesTableViewCell.swift
+//  MarvelOus
+//
+//  Created by Marcela Ceneviva Auslenter on 21/08/2018.
+//  Copyright © 2018 Marcela Ceneviva Auslenter. All rights reserved.
+//
+
+import Foundation
